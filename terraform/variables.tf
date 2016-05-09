e06b5027d651bd9537968d564f016cf3af2aa751
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-05-09 14:10:41 +1000

    Update default values for "vpc_id" and "ecs_cluster_subnet_ids"

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,16 +25,12 @@ variable "vpc_id" {
   	description = "The id of the VPC where the ECS cluster should run"
   	# Set vpc_id to the id of the "Default" VPC from the VPC list: 
 	#	https://ap-southeast-2.console.aws.amazon.com/vpc/home?region=ap-southeast-2#vpcs:
-	default = {
-		vpc_id = "vpc-8c3554e9"
-	}
+	default = "vpc-8c3554e9"
 }
 
 variable "ecs_cluster_subnet_ids" {
   	description = "A comma-separated list of subnets where the EC2 instances for the ECS cluster should be deployed"
   	# Set both subnet_ids lists to the subnet ids (separated with commas) from the subnet list: 
 	#	https://ap-southeast-2.console.aws.amazon.com/vpc/home?region=ap-southeast-2#subnets:
-	default = {
-		ecs_cluster_subnet_ids = "subnet-e51b5080,subnet-02732244,subnet-96bccde1"
-	}
+	default = "subnet-e51b5080,subnet-02732244,subnet-96bccde1"
 }
