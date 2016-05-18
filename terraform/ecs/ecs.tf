491be6dc8061a1a69ad05e46ff16be9660b68702
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-05-18 11:37:18 +1000

    Add Hello World ECS Service and Task Definition

--- terraform/ecs/ecs.tf
+++ terraform/ecs/ecs.tf
@@ -9,3 +9,24 @@ resource "aws_ecs_cluster" "ecs_cluster" {
         create_before_destroy = true
     }
 }
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+	family = "hello-world-task"
+	container_definitions = "${file("task-definitions/hello-world-task.json")}"
+}
+
+## ECS service and task definitions for running container
+resource "aws_ecs_service" "ecs-service" {
+	name = "terraform-ecs-service"
+  	cluster = "${aws_ecs_cluster.ecs_cluster.id}"
+  	task_definition = "${aws_ecs_task_definition.ecs_task_definition.arn}"
+  	desired_count = 1
+  	iam_role = "${aws_iam_role.ecs_service_role.arn}"
+  	depends_on = ["aws_iam_role_policy.ecs_service_policy"]
+
+  	load_balancer {
+    	elb_name = "${aws_elb.ecs_elb.id}"
+    	container_name = "hello-world-container"
+    	container_port = 5000
+  	}
+}
