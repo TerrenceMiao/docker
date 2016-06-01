454d14722bfbd96de4ee3ee3881cfa3663033d7c
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-06-01 15:22:44 +1000

    Add Sumo Logic Collector task and service in Terraform ECS configuration

--- terraform/ecs/ecs.tf
+++ terraform/ecs/ecs.tf
@@ -10,13 +10,13 @@ resource "aws_ecs_cluster" "ecs_cluster" {
     }
 }
 
+## Docker application "Hello World" ECS task and service definitions for running container
 resource "aws_ecs_task_definition" "ecs_task_definition" {
 	family = "hello-world-task"
 	container_definitions = "${file("task-definitions/hello-world-task.json")}"
 }
 
-## ECS service and task definitions for running container
-resource "aws_ecs_service" "ecs-service" {
+resource "aws_ecs_service" "ecs_service" {
 	name = "terraform-ecs-service"
   	cluster = "${aws_ecs_cluster.ecs_cluster.id}"
   	task_definition = "${aws_ecs_task_definition.ecs_task_definition.arn}"
@@ -29,4 +29,22 @@ resource "aws_ecs_service" "ecs-service" {
     	container_name = "hello-world-container"
     	container_port = 5000
   	}
+}
+
+## Docker application "Sumo Logic Collector" ECS task and service definitions for running container
+resource "aws_ecs_task_definition" "sumo_logic_collector_task_definition" {
+	family = "sumo-logic-collector-task"
+	container_definitions = "${file("task-definitions/sumo-logic-collector-task.json")}"
+
+	volume {
+    	name = "sumo-logic-collector-volume"
+    	host_path = "/tmp/clogs"
+  	}
+}
+
+resource "aws_ecs_service" "sumo_logic_collector_service" {
+	name = "sumo-logic-collector-service"
+  	cluster = "${aws_ecs_cluster.ecs_cluster.id}"
+  	task_definition = "${aws_ecs_task_definition.sumo_logic_collector_task_definition.arn}"
+  	desired_count = 1
 }
