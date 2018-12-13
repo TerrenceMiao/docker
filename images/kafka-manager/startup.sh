e5302c281c4eecee9e72810dab5d105097f6ebfb
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2018-12-13 18:21:45 +1100

    Refactor startup.sh script in Kafka Manager

--- images/kafka-manager/startup.sh
+++ images/kafka-manager/startup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ $KM_USERNAME != ''  && $KM_PASSWORD != '' ]]; then
+if [[ "$KM_USERNAME" != ''  && "$KM_PASSWORD" != '' ]]; then
     sed -i.bak '/^basicAuthentication/d' /kafka-manager-${KM_VERSION}/conf/application.conf
     echo 'basicAuthentication.enabled=true' >> /kafka-manager-${KM_VERSION}/conf/application.conf
     echo "basicAuthentication.username=${KM_USERNAME}" >> /kafka-manager-${KM_VERSION}/conf/application.conf
